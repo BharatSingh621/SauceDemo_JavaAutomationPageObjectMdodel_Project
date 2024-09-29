3d12e14068d9eda951a5b7d4052121fcc1a16ce8
Bharat Singh <Bharatsingh621@gmail.com>
2024-09-29 12:12:29 +0530

    Added New Test Case: Sanjana TestCase

---
+++ src/test/java/TestCase/SanjanaTestCase.java
@@ -0,0 +1,12 @@
+package TestCase;
+
+import org.testng.annotations.Test;
+
+public class SanjanaTestCase {
+
+	@Test
+	public void SampleTestCase() 
+	{
+		System.out.println("Hello world");
+	}
+}
